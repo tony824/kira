1728ca6f4b95c2989a82636a861e1d2a56c486fc
Tony <tonywang824@gmail.com>
2018-06-16 09:50:24 -0400

    Support variadic in overlap and use ->>

--- src/kira/core.clj
+++ src/kira/core.clj
@@ -17,41 +17,50 @@
 
 (defn my-compare
   "Determines whether 2 events [l1 r1] [l2 r2] overlap or not
-   strict is false (l1<=r2) and (r1>=l2)
-   strict is true (l1<r2) and (r1>l2)"
+   strict-mode? is false, (l1<=r2) and (r1>=l2)
+   strict-mode? is true, (l1<r2) and (r1>l2)"
   [strict-mode? [l1 r1] [l2 r2]]
   (try
     (if strict-mode?
       (and (neg? (compare l1 r2))
            (pos? (compare r1 l2)))
-      (and (not= (compare l1 r2) 1)
-           (not= (compare r1 l2) -1)))
+      (not (or (pos? (compare l1 r2))
+               (neg? (compare r1 l2)))))
     (catch Exception e
       (println (str "Unexpected error-" (.getMessage ^Exception e)))
       nil)))
 
 (defn overlapped?
-  "Make events comparable"
+  "Make events comparable before comparing"
   [strict-mode? & args]
-  (let [[l1 r1 l2 r2 :as coll] (map date-time (mapcat identity args))]
-    (when (not-any? #(= :invalid %) coll)
-      (my-compare strict-mode? [l1 r1] [l2 r2]))))
+  (let [coll (->> args
+                  (mapcat identity)
+                  (map date-time)
+                  (take 4))]
+    (when-let [[f s] (and (= 4 (count coll))
+                          (not-any? #(= :invalid %) coll)
+                          (partition 2 coll))]
+      (my-compare strict-mode? f s))))
 
 (defn overlap
   "Return overlapped events
    coll is a sequence of [l r]
    strict-mode? overlap strictly or not"
-  [coll strict-mode?]
-  (loop [f (first coll) r (rest coll) acc []]
-    (if (seq r)
-      (let [t (->> r
-                   (filter (partial overlapped? strict-mode? f))
-                   (map (partial list f))
-                   (concat acc))]
-        (recur (first r) (rest r) t))
-      acc)))
+  ([coll]
+   (overlap coll false))
+  ([coll strict-mode?]
+   (loop [f (first coll) r (rest coll) acc []]
+     (if (seq r)
+       (let [t (->> r
+                    (filter (partial overlapped? strict-mode? f))
+                    (map (partial list f))
+                    (concat acc))]
+         (recur (first r) (rest r) t))
+       acc))))
 
 (defn -main [& args]
   []
-  (overlap '([1 2] [2 3] [3 4] [2 5] [2 8]) true)
-  (overlap '([1 2] [2 3] [3 4] [2 5] [2 8]) false))
+  (let [coll '([1 2] [2 3] [3 4] [2 5] [2 8])]
+    (println "INPUT:" coll)
+    (println "Overlapped:"(overlap coll))
+    (println "Overlapped with strict mode:" (overlap coll true))))
